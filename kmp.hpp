5fa0fbf6f31f86761e4b5c3180ede7f8d1da6196
marianoC31 <mariano05055@gmail.com>
2026-09-21 10:43:36 -0600

    elimina otros cpp

--- kmp.hpp
+++ kmp.hpp
@@ -16,12 +16,72 @@ struct Evento {
 };
 
 
-vector<int> construirTablaFallos(const std::string& patron);
+vector<int> construirTablaFallos(const std::string& patron){
+    int N = patron.size();
+
+    vector<int> tabla(N, 0);
+
+    int longitud = 0, i = 0;
+
+    while (i<N){
+        if(patron[i]==patron[longitud]){
+            longitud+=1;
+            tabla[i]=longitud;
+            i++;
+        } else if(longitud!=0){
+            longitud = tabla[longitud-1];
+        } else{
+            tabla[i]=0;
+            i++;
+        }
+    }
+
+    return tabla;
+}
 
 int kmpBuscarPrimera(const std::string& texto,
                       const std::string& patron,
                       std::vector<Evento>* log = nullptr,
                       const std::string& nombreTexto = "",
-                      const std::string& nombrePatron = "");
+                      const std::string& nombrePatron = ""){
+
+        vector<int> lps = construirTablaFallos(patron);
+        int i = 0,j = 0;
+        
+        while(i<texto.size()){
+            if(log!=nullptr){
+                log->push_back({"parte1", nombreTexto, nombrePatron, i, j, "comparando"});
+            }
+            if(texto[i]==patron[j]){
+                if(log!=nullptr){
+                    log->push_back({"parte1", nombreTexto, nombrePatron, i, j, "match_parcial"});
+                }
+                i++;
+                j++;
+            }
+            if(j==patron.size()){
+                if(log!=nullptr){
+                    log->push_back({"parte1", nombreTexto, nombrePatron, i, j, "match_total"});
+                }
+                return i-j;
+            }
+            if(i < texto.size() && texto[i]!=patron[j]){
+                if(log!=nullptr){
+                    log->push_back({"parte1", nombreTexto, nombrePatron, i, j, "mismatch"});
+                }
+                if(j!=0){
+                    j = lps[j-1];
+                }else{
+                    i++;
+                }
+            }
+        }
+        if(log!=nullptr){
+            log->push_back({"parte1", nombreTexto, nombrePatron, i, j, "no encontrado"});
+        }
+
+        return -1;
+
+                      }
 
 #endif
