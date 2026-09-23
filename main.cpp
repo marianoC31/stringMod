d47a5fab5233bdf5edab0218bed7ae595a13cf91
hope <81796354+esperanzaaroman@users.noreply.github.com>
2026-09-23 03:56:37 -0600

    me voy a matar

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include "kmp.hpp"
+#include "manacher.hpp"
 
 using namespace std;
 
---
+++ manacher.hpp
@@ -0,0 +1,55 @@
+#ifndef MANACHER_HPP
+#define MANACHER_HPP
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void manacher(string texto) {
+
+    string t = "#";
+    for (int i = 0; i < texto.length(); i++) {
+        t += texto[i];
+        t += "#";
+    }
+
+    vector<int> P(t.length(),0);
+    int C;
+    int R;
+
+    for (int i = 0; i < t.length(); i++) {
+
+        int mirror = 2*C-i;
+        if (i < R){
+            P[i] = min(P[mirror], R-i);
+        }
+
+        while((i-P[i] - 1 >= 0) && (i + P[i] + 1 < t.length()) && t[i-P[i]-1] == t[i+P[i]+1]) {
+            P[i]++;   
+        }
+
+        if (i+P[i]>R) {
+            C = i;
+            R = i + P[i];
+        }
+    }
+
+    int maxLen= 0;
+    int centro = 0;
+
+    for (int i = 0; i < P.size(); i++) {
+        if (P[i] > maxLen) {
+            maxLen=P[i];
+            centro = i;
+        }
+    }
+
+    int inicio = (centro - maxLen) / 2+1;
+    int fin = (centro +maxLen) / 2;
+    cout << inicio << " " << fin << endl;
+
+};
+
+#endif
